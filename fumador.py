03a5cd356e01c8af9e6162c4cb9493dd249c72ce
Ana López <anamuyamiga@gmail.com>
2023-04-29 19:03:34 +0200

    comentarios

--- fumador.py
+++ fumador.py
@@ -5,14 +5,14 @@ from storage import packet_size, store, time_sleep, time_smoke
 from utils import _print
 
 def proceso(code, request):
-    mensaje = ""
+    mensaje = "" #Mensaje que va cambiando con el que nos comunicamos al servidor
     while True:
         if mensaje != 'ack':
-            _print("Esperando {}!".format(store.get(code)['required']))
-            request.send('need'.encode('UTF-8'))
+            _print("Esperando {}!".format(store.get(code)['required'])) #Espero por el ingrediente (recurso) requerido
+            request.send('need'.encode('UTF-8')) #Envío la petición del recurso al servidor
         
-        mensaje = request.recv(packet_size).decode('UTF-8')
-        if mensaje == 'enable':
+        mensaje = request.recv(packet_size).decode('UTF-8') #Espero por la respuesta del servidor y la almaceno en la variable
+        if mensaje == 'enable': #Si el mensaje recibido es enable, es que ya tengo todo para fumar
             _print('¡Servido!')
             time.sleep(time_sleep)
             request.send('ack'.encode('UTF-8'))
@@ -25,3 +25,5 @@ def proceso(code, request):
         elif mensaje == 'ack':
             pass
         time.sleep(time_sleep)
+
+
