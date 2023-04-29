67a6631ad9c6d7d1ea1557da9f4fe2ed4dd75fd2
Ana López <anamuyamiga@gmail.com>
2023-04-29 20:31:44 +0200

    main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 from storage import codes, store
 
-def get_puerto():
+def get_puerto(): #Defino el puerto
     while True:
         try:
             puerto = int(input('Puerto (1024 - 49151): '))
@@ -11,3 +11,33 @@ def get_puerto():
         except Exception:
             pass
     return puerto
+
+#CODIGO PRINCIPAL
+if __name__ == '__main__':
+    os.system('clear')
+    while True:
+        print('1. Proveedor')
+        print('2. Fumador (5 opciones)')
+        type = input('Opcion: ')
+        if type in ['1', '2']:
+            break
+        else:
+            os.system('clear')
+    os.system('clear')
+    if type == '1':
+        print('Proveedor')
+        from proveedor import init
+        init(get_puerto())
+    else:
+        while True:
+            print('Fumador')
+            for i in codes:
+                print('{}. {}'.format(i, store[i].get('name')))
+            type = input('Opcion: ')
+            if type in codes:
+                break
+            else:
+                os.system('clear')
+        ip = input('IP del proveedor: ')
+        from fumador import init
+        init(ip, get_puerto(), type)
