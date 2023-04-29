0593366a5a326d6f30db0f1d48389f6d3a5b0a20
Ana López <anamuyamiga@gmail.com>
2023-04-29 19:57:12 +0200

    comentarios handle

--- proveedor.py
+++ proveedor.py
@@ -38,10 +38,10 @@ class MyTCPServerHandler(socketserver.BaseRequestHandler): #Clase que creo para 
 
     def handle(self):#Funcion para manejar el servidor
         self.code = self.request.recv(packet_size).decode('UTF-8')
-        self.rejectd = False
+        self.rejected = False
         self.smoke_released = False
-        _print('Conectando fumador...')
-        if store.get(self.code)['flag'] is False:
+        _print('Conectando fumador...') #Intentamos conectar con el fumador tras su mensaje.
+        if store.get(self.code)['flag'] is False: #Se conecta si flag es False
             store.get(self.code)['request'] = self.request
             store.get(self.code)['flag'] = True
             _print('Fumador aceptado *{}*'.format(store.get(self.code)['name']))
@@ -51,4 +51,3 @@ class MyTCPServerHandler(socketserver.BaseRequestHandler): #Clase que creo para 
             self.rejected = True
             _print('Fumador rechazado *{}*'.format(store.get(self.code)['name']))
             self.request.send('rejected'.encode('UTF-8'))
-            
