af8141b9dbada2b27c44aea5e09280cf06fa1ea7
Ana López <anamuyamiga@gmail.com>
2023-04-29 18:55:39 +0200

    funcion proceso de fumador

--- fumador.py
+++ fumador.py
@@ -3,3 +3,25 @@ import time
 
 from storage import packet_size, store, time_sleep, time_smoke
 from utils import _print
+
+def proceso(code, request):
+    mensaje = ""
+    while True:
+        if mensaje != 'ack':
+            _print("Esperando {}!".format(store.get(code)['required']))
+            request.send('need'.encode('UTF-8'))
+        
+        mensaje = request.recv(packet_size).decode('UTF-8')
+        if mensaje == 'enable':
+            _print('¡Servido!')
+            time.sleep(time_sleep)
+            request.send('ack'.encode('UTF-8'))
+            _print('Armando cigarro')
+            time.sleep(time_sleep)
+            _print('Fumando')
+            time.sleep(time_smoke)
+            request.send('enable'.encode('UTF-8'))
+
+        elif mensaje == 'ack':
+            pass
+        time.sleep(time_sleep)
