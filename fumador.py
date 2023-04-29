ac1b81c9e63b84bfa2be821e978adf7feef862eb
Ana López <anamuyamiga@gmail.com>
2023-04-29 19:20:46 +0200

    comentarios init

--- fumador.py
+++ fumador.py
@@ -4,7 +4,7 @@ import time
 from storage import packet_size, store, time_sleep, time_smoke
 from utils import _print
 
-def proceso(code, request):
+def proceso(code, request): #Función que manda los distintos mensajes posibles del fumador al servidor y lo ejecuta.
     mensaje = "" #Mensaje que va cambiando con el que nos comunicamos al servidor
     while True:
         if mensaje != 'ack':
@@ -27,16 +27,16 @@ def proceso(code, request):
         time.sleep(time_sleep)
 
 
-def init(ip, puerto, code):
+def init(ip, puerto, code): #Funcion para comunicarse con el servidor
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((ip, puerto))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Creo un socket, la conexión.
+        sock.connect((ip, puerto)) #Me conecto a la ip y el servidor del socket
 
-        sock.send('{}'.format(code).encode('UTF-8'))
+        sock.send('{}'.format(code).encode('UTF-8')) #Envío la petición al servidor
         time.sleep(time_sleep)
 
-        respuesta = sock.recv(packet_size).decode('UTF-8')
-        if respuesta == 'accepte':
+        respuesta = sock.recv(packet_size).decode('UTF-8') #Espero por su respuesta 
+        if respuesta == 'accepte': #Si me deja entrar, ejecuto la funcion proceso en ese socket
             proceso(code, sock)
         else:
             _print('Rechazado por el proveedor')
